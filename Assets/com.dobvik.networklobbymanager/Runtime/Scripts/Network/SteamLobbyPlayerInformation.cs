5bcacac2b35e7db3dfce120f65ff9bbbe4c6df99
Ruslan <nukhov2004@bk.ru>
2026-05-26 13:46:48 +0500

    Added avatar

--- Assets/com.dobvik.networklobbymanager/Runtime/Scripts/Network/SteamLobbyPlayerInformation.cs
+++ Assets/com.dobvik.networklobbymanager/Runtime/Scripts/Network/SteamLobbyPlayerInformation.cs
@@ -9,13 +9,16 @@ namespace NetworkLobbyManager
     {
         [Header("Callbacks")]
         private Callback<PersonaStateChange_t> personaStateChange;
+        private Callback<AvatarImageLoaded_t> avatarImageLoaded;
 
         [Header("Events")]
         [HideInInspector] public UnityEvent<SteamLobbyPlayerInformation> onSteamIDChanged;
         [HideInInspector] public UnityEvent<SteamLobbyPlayerInformation> onPlayerSteamNameChanged;
+        [HideInInspector] public UnityEvent<SteamLobbyPlayerInformation> onPlayerSteamAvatarChanged;
 
         public CSteamID SteamID => steamID;
         public string PlayerSteamName => playerSteamName;
+        public int PlayerSteamAvatar => playerSteamAvatar;
 
         #region SyncVars
 
@@ -25,6 +28,9 @@ namespace NetworkLobbyManager
 
         [SyncVar(hook = nameof(PlayerSteamNameChanged))]
         [SerializeField, ReadOnly] private string playerSteamName;
+        
+        [SyncVar(hook = nameof(PlayerSteamAvatarChanged))]
+        [SerializeField, ReadOnly] private int playerSteamAvatar;
 
         private void SteamIDChanged(CSteamID _, CSteamID newSteamID)
         {
@@ -34,6 +40,11 @@ namespace NetworkLobbyManager
         private void PlayerSteamNameChanged(string _, string newPlayerSteamName)
         {
             onPlayerSteamNameChanged?.Invoke(this);
+        }
+        
+        private void PlayerSteamAvatarChanged(int _, int newPlayerSteamAvatar)
+        {
+            onPlayerSteamAvatarChanged?.Invoke(this);
         }
 
         #endregion
@@ -48,11 +59,13 @@ namespace NetworkLobbyManager
             }
             
             personaStateChange = Callback<PersonaStateChange_t>.Create(OnPersonaStateChange);
+            avatarImageLoaded = Callback<AvatarImageLoaded_t>.Create(OnAvatarImageLoaded);
         }
 
         public void OnDisable()
         {
             personaStateChange?.Dispose();
+            avatarImageLoaded?.Dispose();
         }
 
         #endregion
@@ -78,6 +91,19 @@ namespace NetworkLobbyManager
             {
                 playerSteamName = SteamFriends.GetFriendPersonaName(steamID);
             }
+            else if (callback.m_ulSteamID == steamID.m_SteamID && callback.m_nChangeFlags == EPersonaChange.k_EPersonaChangeAvatar)
+            {
+                playerSteamAvatar = SteamFriends.GetLargeFriendAvatar(steamID);
+            }
+        }
+        
+        [ServerCallback]
+        private void OnAvatarImageLoaded(AvatarImageLoaded_t callback)
+        {
+            if (callback.m_steamID.m_SteamID == steamID.m_SteamID)
+            {
+                playerSteamAvatar = callback.m_iImage;
+            }
         }
 
         #endregion
@@ -88,6 +114,7 @@ namespace NetworkLobbyManager
         private void OnSteamIDChanged()
         {
             playerSteamName = SteamFriends.GetFriendPersonaName(steamID);
+            playerSteamAvatar = SteamFriends.GetLargeFriendAvatar(steamID);
         }
 
         #endregion
